2631fa06428a1dcaabc0ccb11b87c1aed9997421
ismailpgithub <ismail.p1721@gmail.com>
2025-09-08 18:56:33 +0530

    Changed the config.js according to the prod

--- BACKEND/src/config/config.js
+++ BACKEND/src/config/config.js
@@ -1,6 +1,6 @@
 export const cookieOptions = {
-    httpOnly: true,
-    secure: process.env.NODE_ENV === "production",
-    sameSite: "strict",
-    maxAge: 1000 * 60 * 60, // 5 minutes
-}
+  httpOnly: true,
+  secure: process.env.NODE_ENV === "production",
+  sameSite: process.env.NODE_ENV === "production" ? "strict" : "lax",
+  maxAge: 1000 * 60 * 60, // 1hr
+};
