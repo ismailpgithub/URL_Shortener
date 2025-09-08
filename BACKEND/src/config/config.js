34557fffdc617fadbee84234b0920205ca6870ca
ismailpgithub <ismail.p1721@gmail.com>
2025-09-08 22:12:51 +0530

    Changed none from lax in config.js

--- BACKEND/src/config/config.js
+++ BACKEND/src/config/config.js
@@ -1,6 +1,6 @@
 export const cookieOptions = {
   httpOnly: true,
   secure: process.env.NODE_ENV === "production",
-  sameSite: process.env.NODE_ENV === "production" ? "strict" : "lax",
+  sameSite: process.env.NODE_ENV === "production" ? "strict" : "none",
   maxAge: 1000 * 60 * 60, // 1hr
 };
