035ffd7f28f0de9c7824340ad434ab2bd8825398
ismailpgithub <ismail.p1721@gmail.com>
2025-09-08 23:37:18 +0530

    Add production in env variable

--- BACKEND/src/config/config.js
+++ BACKEND/src/config/config.js
@@ -1,6 +1,6 @@
 export const cookieOptions = {
   httpOnly: true,
-  secure: true,
-  sameSite: "none",
+  secure: process.env.NODE_ENV === "production",
+  sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
   maxAge: 1000 * 60 * 60, // 1hr
 };
